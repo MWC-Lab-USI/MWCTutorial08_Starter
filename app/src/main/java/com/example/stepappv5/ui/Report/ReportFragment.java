743b200da8bba6a2b93277988ea71ac8a5d5a73c
Leonardo Alchieri <leonardo.alchieri@usi.ch>
2024-11-20 14:32:39 +0100

    Removed code form todos

---
+++ app/src/main/java/com/example/stepappv5/ui/Report/ReportFragment.java
@@ -0,0 +1,120 @@
+package com.example.stepappv5.ui.Report;
+
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+import com.example.stepappv5.R;
+import com.example.stepappv5.StepAppOpenHelper;
+import com.example.stepappv5.databinding.FragmentReportBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ReportFragment extends Fragment {
+
+    public int todaySteps = 0;
+    TextView numStepsTextView;
+    AnyChartView anyChartView;
+
+    Date cDate = new Date();
+    String current_time = new SimpleDateFormat("yyyy-MM-dd").format(cDate);
+
+    public Map<Integer, Integer> stepsByHour = null;
+
+    private FragmentReportBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentReportBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        // Create column chart
+        anyChartView = root.findViewById(R.id.hourBarChart);
+        anyChartView.setProgressBar(root.findViewById(R.id.loadingBar));
+
+        Cartesian cartesian = createColumnChart();
+        anyChartView.setBackgroundColor("#00000000");
+        anyChartView.setChart(cartesian);
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    public Cartesian createColumnChart(){
+        //***** Read data from SQLiteDatabase *********/
+        stepsByHour = StepAppOpenHelper.loadStepsByHour(getContext(), current_time);
+
+
+        Map<Integer, Integer> graph_map = new TreeMap<>();
+        for(int i =0; i<24; i++){
+            graph_map.put(i, 0);
+        }
+
+        graph_map.putAll(stepsByHour);
+
+        //***** Create column chart using AnyChart library *********/
+        Cartesian cartesian = AnyChart.column();
+
+        List<DataEntry> data = new ArrayList<>();
+
+        for (Map.Entry<Integer,Integer> entry : graph_map.entrySet())
+            data.add(new ValueDataEntry(entry.getKey(), entry.getValue()));
+
+        Column column = cartesian.column(data);
+
+        //***** Modify the UI of the chart *********/
+        column.fill(String.valueOf(androidx.appcompat.R.attr.colorPrimary));
+        column.stroke(String.valueOf(com.google.android.material.R.attr.colorPrimary));
+
+        column.tooltip()
+                .titleFormat("At hour: {%X}")
+                .format("{%Value} Steps")
+                .anchor(Anchor.RIGHT_BOTTOM);
+
+        column.tooltip()
+                .position(Position.RIGHT_TOP)
+                .offsetX(0d)
+                .offsetY(5);
+
+        // Modifying properties of cartesian
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+        cartesian.yScale().minimum(0);
+
+        cartesian.yAxis(0).title("Number of steps");
+        cartesian.xAxis(0).title("Hour");
+//        we are setting the background to white, but with full transparency
+        cartesian.background().fill(String.valueOf("#FFFFFF"));
+        cartesian.animation(true);
+
+        return cartesian;
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.7.1" apply false
+}
+
+tasks.register("clean",Delete::class){
+    delete(rootProject.buildDir)
+}
+buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
